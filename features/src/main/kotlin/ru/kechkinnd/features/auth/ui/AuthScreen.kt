b3f452408b4f5370e0e4f2a7617b1115ef510a60
Nikolay <wolk20000@g,ail.com>
2025-07-09 17:53:59 +0300

    1 step

--- features/src/main/kotlin/ru/kechkinnd/features/auth/ui/AuthScreen.kt
+++ features/src/main/kotlin/ru/kechkinnd/features/auth/ui/AuthScreen.kt
@@ -1,19 +1,73 @@
 package ru.kechkinnd.features.auth.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun AuthScreen(onLoginSuccess: () -> Unit) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+fun AuthScreen(
+    viewModel: AuthViewModel = viewModel(),
+    onAuthSuccess: () -> Unit
+) {
+    val state by viewModel.uiState.collectAsState()
+    val uriHandler = LocalUriHandler.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Auth screen")
+        OutlinedTextField(
+            value = state.email,
+            onValueChange = viewModel::onEmailChange,
+            label = { Text("Email") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = state.password,
+            onValueChange = viewModel::onPasswordChange,
+            label = { Text("Пароль") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = { onAuthSuccess() },
+            enabled = state.isEmailValid,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Войти")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+            OutlinedButton(onClick = {
+                uriHandler.openUri("https://vk.com")
+            }) {
+                Text("VK")
+            }
+
+            OutlinedButton(onClick = {
+                uriHandler.openUri("https://ok.ru")
+            }) {
+                Text("OK")
+            }
+        }
     }
 }
-
